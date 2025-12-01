ac95cebd697aaadf511488d589162030fa2cf915
Victor Micha <victor.micha@mail.mcgill.ca>
2025-12-01 21:38:05 +0100

    pre-training progress: manually generated labels + loss calculated + eos and padding

--- src/training/pre_training.py
+++ src/training/pre_training.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset, Dataset
 from src.model.vic_gpt import VicGPT
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, DataCollatorWithPadding
 import torch
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
@@ -26,7 +26,7 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
     tokenizer.pad_token = tokenizer.eos_token # gpt2 has no pad_token
     print(f"Tokenizer {tokenizer_name} loaded")
-
+    print(f"eos id:{tokenizer.eos_token_id}, pad: {tokenizer.pad_token_id}")
     ########################################################
     max_seq_len=128 # TODO THIS SHOULD BE INPUT TO THE SCRIPT USING ARGSPARSE
     num_proc = 8 # number of processes for tokenizing dataset
@@ -54,18 +54,23 @@ def main():
         """
         tokenizer() returns a dict of {"input_ids":..., "attention_mask":...}
         """
-        return tokenizer(examples["text"], truncation=True, max_length=max_seq_len)
+        # need to add <eos> token at end of each sequence in case of truncation
+        # this way the model will learn to end its generation with <eos>
+        result = tokenizer(examples["text"], truncation=True, max_length=max_seq_len-1)#-1 to leave space for <eos>
+        result["input_ids"] = [ids + [tokenizer.eos_token_id] for ids in result["input_ids"]]
+        result["attention_mask"] = [mask + [1] for mask in result["attention_mask"]]
+        return result
     ########################
     ds["train"] = ds["train"].select(range(10)) # TODO FOR TESTING ONLY, comment out
     ds["validation"] = ds["validation"].select(range(10))# TODO FOR TESTING ONLY, COMMENT OUT
     #######################
     print("Tokenizing entire dataset:")
     tokenized_ds = ds.map(tokenize, batched=True, num_proc=num_proc, remove_columns=["text"])
-    print(tokenized_ds)
-
+    #print(tokenized_ds["train"][0]["input_ids"])
+    #print(tokenized_ds["train"][0]["attention_mask"])
 
     # sanity check, decoding the input ids should be equal to the original text
-    decoded_test_string = tokenizer.decode(tokenized_ds["train"][0]["input_ids"])
+    decoded_test_string = tokenizer.decode(tokenized_ds["train"][0]["input_ids"][:-1])
     assert decoded_test_string == ds["train"][0]["text"][:len(decoded_test_string)]
     print("Tokenization successfull")
 
@@ -74,41 +79,54 @@ def main():
     tokenized_ds.set_format(type="torch", columns=["input_ids", "attention_mask"])
     #print(tokenized_ds["train"][0]["input_ids"])
 
+    # need data collator to ensure all sequences in a batch have the same length by padding the shorter ones
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+
     train_loader = DataLoader(
         tokenized_ds["train"],
         batch_size=train_batch_size,
         shuffle=True,
-        num_workers=0
+        num_workers=0,
+        collate_fn=data_collator
     )
 
     val_loader = DataLoader(
         tokenized_ds["validation"],
         batch_size=val_batch_size,
         shuffle=False,
-        num_workers=0
+        num_workers=0,
+        collate_fn=data_collator
     )
-
-    #print(train_loader[0])
     optimizer = AdamW(vicGPT.parameters(), lr=lr, weight_decay=weight_decay)
     vicGPT.to(device)
     print(f"Using device {device}")
     
     train_losses = []
     val_losses = []
-    
+    print("Starting training") 
     for epoch in range(num_epochs):
         epoch_loss = 0
         vicGPT.train()
         for batch_idx, batch in enumerate(train_loader):
             
             input_ids = batch["input_ids"].to(device) # (batch_size, seq_len)
+            attention_mask = batch["attention_mask"] # (batch_size, seq_len) TODO NEED TO PUT ON DEVICE TOO?
             print(f"batch id {batch_idx}")
             #print(input_ids.shape) 
             logits = vicGPT(input_ids) # (batch_size, seq_len, vocab_size)
-            print(logits)
-            print(logits.shape)
-            # TODO loss = F.cross_entropy()
-            return
+            # now need to generate labels
+            labels = input_ids.clone()
+            #print(attention_mask)
+            labels[attention_mask == 0] = -100 # set labels to -100 at padding locations to not include in loss
+            # need to shift labels and logits so that logits[i] predict labels[i+1] (next token prediction)
+            labels = labels[:, 1:] # labels at positions 1 to seq_len-1
+            logits = logits[:, :-1, :] # logits at positions 0 to seq_len-2
+            # need to reshape for cross_entropy
+            labels = labels.reshape(-1) # (batch_size * seq_len-1)
+            logits = logits.reshape(-1, tokenizer.vocab_size) # (batch_size * seq_len-1, vocab_size)
+            loss = F.cross_entropy(logits, labels, ignore_index=-100)
+            # TODO, STILL NEED TO VERIFY THAT TRUNCATION + PADDING + ATTENTION MASKS ARE ALL WORKING WELL
+            # TODO, FOR THIS COULD TRY WITH A VERY SMALL MAX SEQ LEN AND 1 EXAMPLE 
             optimizer.zero_grad() 
             loss.backward()
             optimizer.step()
@@ -120,13 +138,9 @@ def main():
         # TODO vicGPT.eval(), do validation loss
         # val_loss = 0
         #with torch.no_grad():
-        
+    print(train_losses)
     print("Done!")
     # TODO SAVE MODEL, MAYBE THE ONE WITH LOWEST VAL LOSS?
             
 if __name__ == "__main__":
     main()
-# USE DATALOADER AND BATCHING
-# TRAIN FOR EVERY EPOCH
-# also every var, including batch size should be export in the pre_training.sh in the root directory
-print("done")
